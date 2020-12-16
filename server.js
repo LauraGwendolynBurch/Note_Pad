5c5a762b8ef4e8b3db99a8ead83eb279f55ccfc2
Laura Gwen <lauraburch.lb@gmail.com>
2020-12-15 23:07:21 -0500

    added server.js and opening code

---
+++ server.js
@@ -0,0 +1,18 @@
+var express = require("express");
+var path = require("path");
+
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static('public'))
+
+app.get("/", function(req, res) {
+    res.send("hello world!!!!!!");
+  });
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
